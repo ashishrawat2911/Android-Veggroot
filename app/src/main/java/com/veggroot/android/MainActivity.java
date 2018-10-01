1ae584f7d4ebc50ed7f835f59d80d5668441ccb9
Ashish Rawat <ashishrawat2911@gmail.com>
2018-10-01 19:51:55 +0530

    removed firebase from the entire project

--- app/src/main/java/com/veggroot/android/MainActivity.java
+++ app/src/main/java/com/veggroot/android/MainActivity.java
@@ -1,15 +1,23 @@
 package com.veggroot.android;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
+    ProgressBar progressBar;
+    String URL = "http://veggroot.godaddysites.com/";
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-}
+        setContentView(R.layout.activity_main);}}
