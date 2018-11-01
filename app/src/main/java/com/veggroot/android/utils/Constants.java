871071489763c41e06faa623452f03932a4c527d
Ashish Rawat <ashishrawat2911@gmail.com>
2018-11-01 11:28:58 +0530

    removed constants fields

--- app/src/main/java/com/veggroot/android/utils/Constants.java
+++ app/src/main/java/com/veggroot/android/utils/Constants.java
@@ -2,6 +2,4 @@ package com.veggroot.android.utils;
 
 public class Constants {
     public static final String VEGGROOT_URL = "http://veggroot.com/";
-    public static String TOTAL_ITEMS = "totalItems";
-    public static String TOTAL_ITEM_COST = "totalItemCost";
 }
