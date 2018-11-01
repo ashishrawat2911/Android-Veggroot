1149834cb2fef0a70e931f6e856669a7ce47a71d
Ashish Rawat <ashishrawat2911@gmail.com>
2018-11-01 11:28:14 +0530

    set the previous address

--- app/src/main/java/com/veggroot/android/activities/EditAddressActivity.java
+++ app/src/main/java/com/veggroot/android/activities/EditAddressActivity.java
@@ -1,20 +1,25 @@
 package com.veggroot.android.activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.veggroot.android.R;
 
 public class EditAddressActivity extends AppCompatActivity {
     EditText address, pincode;
     private FirebaseAuth mAuth;
-    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabase, databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,27 @@ public class EditAddressActivity extends AppCompatActivity {
         address = findViewById(R.id.editAddress);
         pincode = findViewById(R.id.editPinCode);
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
+        setDetails();
+    }
+
+    private void setDetails() {
+        databaseReference.child("user")
+                .child(mAuth.getUid())
+                .child("info").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                address.setText("" + dataSnapshot.child("address").getValue());
+                pincode.setText("" + dataSnapshot.child("pincode").getValue());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(EditAddressActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void saveDetails(View view) {
