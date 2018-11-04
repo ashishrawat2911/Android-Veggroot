c03dd88cd9a263b8f9c08f86d26e15142b42437f
Ashish <ashishrawat2911>
2018-11-04 23:15:26 +0530

    ADDED IMAGES

--- app/src/main/java/com/veggroot/android/activities/OrdersActivity.java
+++ app/src/main/java/com/veggroot/android/activities/OrdersActivity.java
@@ -1,15 +1,63 @@
 package com.veggroot.android.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.veggroot.android.R;
+import com.veggroot.android.adaptor.CartAdaptor;
+import com.veggroot.android.model.Cart;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrdersActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
+    RecyclerView ordersRecyclerView;
+    CartAdaptor cartAdaptor;
+    List<Cart> categoriesList = new ArrayList<>();
+    LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Orders");
+
+        ordersRecyclerView = findViewById(R.id.ordersRecyclerView);
+        linearLayout = findViewById(R.id.noItemsInOrder);
+
+        ordersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ordersRecyclerView.setHasFixedSize(true);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public void startShopping(View view) {
+        startActivity(new Intent(this, MainCategoryActivity.class));
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
